f18b439d7ba39286c8bbc80032f9cf849f9be2b2
PoojanParikh <camkampar@gmail.com>
2017-02-06 11:11:19 +0530

    Blank Commit for JSON Parsing Task

---
+++ AndroidStudioProjects/JSONParsingTask/app/src/main/java/com/poojan/jsonparsingtask/MainActivity.java
@@ -0,0 +1,18 @@
+package com.poojan.jsonparsingtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
